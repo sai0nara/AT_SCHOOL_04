ccdc24cdf8f7ecc9606197d3eef4a28130b06dbb
Ivan <VTB4021906@vtb.ru>
2021-12-20 19:10:13 +0300

    [/] fixed Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,6 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-import somepackage.Other;
-import somepackage.Some;
 import somethinggreen.Contract;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,20 +10,19 @@ map.put("key1", "value");
 map.put("key2", "value");
 map.put("key3", "value");
 map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
+map.put("key4", "value");
 
-        /*LinkedList list =
-                new LinkedList();
-        list.add("UIGIU");
-        list.add(new Object());
-        list.add(1);
 
-        for(int i=0; i<list.size();i++){
-            if(list.get(i).getClass().getSimpleName().equals("Object")){
-                System.out.println(list.get(i));
-            }
-        }
-
-        System.out.println(list.get(0));*/
         for (String s : map.keySet()) {
             System.out.println(map.get(s));
         }
